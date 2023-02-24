39c0995a4b66a6bcd8d231262ab43e60a67558a6
Terrie Ann <malobaterryann@gmail.com>
2023-02-23 18:06:12 -0800

    updated main.py

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from .models import User
 from . import db
 
 main = Blueprint('main', __name__)
-
+location="Select location. For Lavington press 4. For kileleshwa press 5. For Westlands press 6"
 
 username = "sandbox"    # use 'sandbox' for development in the test environment
-api_key = "key"
+api_key = "api_key"
 africastalking.initialize(username, api_key)
 
 
@@ -33,28 +33,63 @@ def voice():
         if keypad_res == '1':
             response = '<?xml version="1.0" encoding="UTF-8"?>'
             response += '<Response>'
-            response += '<Say></Say>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>Select location. For Lavington press 4. For kileleshwa press 5. For Westlands press 6</Say>'
+            response += '</GetDigits>'
             response += '</Response>'
 
         elif keypad_res == '2':
             response = '<?xml version="1.0" encoding="UTF-8"?>'
             response += '<Response>'
-            response += '<Say>You selected option two</Say>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>Select location. For Lavington press 4. For kileleshwa press 5. For Westlands press 6</Say>'
+            response += '</GetDigits>'
             response += '</Response>'
 
         elif keypad_res == '3':
             response = '<?xml version="1.0" encoding="UTF-8"?>'
             response += '<Response>'
-            response += '<Say>You selected option three</Say>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>=Select location. For Lavington press 4. For kileleshwa press 5. For Westlands press 6</Say>'
+            response += '</GetDigits>'
             response += '</Response>'
 
         elif keypad_res == '4':
             response = '<?xml version="1.0" encoding="UTF-8"?>'
             response += '<Response>'
-            response += '<Say>You selected option four</Say>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>Select option 7 for morning or 8 for evening</Say>'
+            response += '</GetDigits>'
             response += '</Response>'
-            sms.send("Successfully booked cleaning session", ["+254777287562"])
-
+            sms.send("Successfully booked cleaning session", ["+254790143657"])
+        elif keypad_res == '5':
+            response = '<?xml version="1.0" encoding="UTF-8"?>'
+            response += '<Response>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>Select option 7 for morning or 8 for evening</Say>'
+            response += '</GetDigits>'
+            response += '</Response>'
+            sms.send("Successfully booked cleaning session", ["+254790143657"])
+        elif keypad_res == '6':
+            response = '<?xml version="1.0" encoding="UTF-8"?>'
+            response += '<Response>'
+            response += '<GetDigits finishOnKey="#">'
+            response += '<Say>Select option 7 for morning or 8 for evening</Say>'
+            response += '</GetDigits>'
+            response += '</Response>'
+            sms.send("Successfully booked cleaning session", ["+254790143657"])
+        elif keypad_res == '7':
+            response = '<?xml version="1.0" encoding="UTF-8"?>'
+            response += '<Response>'
+            response += '<Say>You have been booked for a morning appointment,we will send an sms confirmation</Say>'
+            response += '</Response>'
+            sms.send("Successfully booked cleaning session", ["+254790143657"])
+        elif keypad_res == '8':
+            response = '<?xml version="1.0" encoding="UTF-8"?>'
+            response += '<Response>'
+            response += '<Say>You have been booked for an evening appointment, we will send an sms confirmation</Say>'
+            response += '</Response>'
+            sms.send("Successfully booked cleaning session", ["+254790143657"])    
 
     else:
         duration = request.values.get('durationInSeconds')
